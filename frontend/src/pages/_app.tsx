984946cfbbc00e8665a657e68abfcc30db09c2fd
우동진 <wjinh93@gmail.com>
2022-10-28 15:24:38 +0900

    [#9] build: react-query devtools 설치 및 app에 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createStore } from "redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import GlobalStyle from "@styles/globalstyle";
 import styled, { ThemeProvider } from "styled-components";
-import theme from "@styles/theme";
 import Navbar from "@components/navbar/Navbar";
-import type { AppProps } from "next/app";
-import { persistedReducer, wrapper } from "src/store";
+import GlobalStyle from "@styles/globalstyle";
+import theme from "@styles/theme";
 import "../styles/global.css";
 import "react-toastify/dist/ReactToastify.css";
+import type { AppProps } from "next/app";
+import { persistedReducer, wrapper } from "src/store";
+
 const store = createStore(persistedReducer);
 export const persistor = persistStore(store);
 
@@ -30,6 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 							<ToastContainer style={{ fontSize: "1.4rem" }} />
 						</ThemeProvider>
 					</Container>
+					<ReactQueryDevtools initialIsOpen={true} position="top-right" />
 				</QueryClientProvider>
 			</PersistGate>
 		</Provider>
