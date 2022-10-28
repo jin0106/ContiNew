4055e7464c79d63c6d6a90285b1218129d5f498b
우동진 <wjinh93@gmail.com>
2022-10-28 15:19:27 +0900

    [#9] build: react-query 설치 및 app에 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,32 +1,36 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createStore } from "redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import GlobalStyle from "@styles/globalstyle";
 import styled, { ThemeProvider } from "styled-components";
 import theme from "@styles/theme";
 import Navbar from "@components/navbar/Navbar";
 import type { AppProps } from "next/app";
 import { persistedReducer, wrapper } from "src/store";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import { ToastContainer } from "react-toastify";
 import "../styles/global.css";
 import "react-toastify/dist/ReactToastify.css";
-
 const store = createStore(persistedReducer);
 export const persistor = persistStore(store);
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-				<Container>
-					<GlobalStyle />
-					<ThemeProvider theme={theme}>
-						{Component.displayName !== "term" && <Navbar />}
-						<Component {...pageProps} />
-						<ToastContainer style={{ fontSize: "1.4rem" }} />
-					</ThemeProvider>
-				</Container>
+				<QueryClientProvider client={queryClient}>
+					<Container>
+						<GlobalStyle />
+						<ThemeProvider theme={theme}>
+							{Component.displayName !== "term" && <Navbar />}
+							<Component {...pageProps} />
+							<ToastContainer style={{ fontSize: "1.4rem" }} />
+						</ThemeProvider>
+					</Container>
+				</QueryClientProvider>
 			</PersistGate>
 		</Provider>
 	);
