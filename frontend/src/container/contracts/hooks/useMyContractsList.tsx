fa001873f9c9549eb1b153ec30e82e5e7d1e824a
9603100 <imgoing222@gmail.com>
2022-11-12 21:42:19 +0900

    refactor: useMyContractsList 생성 후 contract 페이지에서 사용

---
+++ frontend/src/container/contracts/hooks/useMyContractsList.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { contractApi } from "src/api";
+import { MyContracts } from "src/types/MyContracts";
+import cookie from "react-cookies";
+import { useRouter } from "next/router";
+
+const useMyContractsList = () => {
+	const accessToken = cookie.load("access_token");
+	const router = useRouter();
+
+	const [onGoingContracts, setOnGoingContracts] = useState<MyContracts[]>([]);
+	const [completedContracts, setCompletedContracts] = useState<MyContracts[]>([]);
+	const [currentTab, setCurrentTab] = useState(0);
+
+	const tabs = ["계약중", "계약완료"];
+
+	useEffect(() => {
+		if (accessToken) getMyContracts();
+	}, []);
+
+	const getMyContracts = async () => {
+		const res = await contractApi.getMyContracts();
+		res.data.forEach((contract: MyContracts) => {
+			if (contract.current_level === 4) setCompletedContracts((prev) => [...prev, contract]);
+			else setOnGoingContracts((prev) => [...prev, contract]);
+		});
+	};
+
+	const goToSignIn = () => {
+		router.push("/account/signin");
+	};
+
+	return {
+		accessToken,
+		setCurrentTab,
+		tabs,
+		currentTab,
+		onGoingContracts,
+		completedContracts,
+		goToSignIn,
+	};
+};
+export default useMyContractsList;
--- frontend/src/pages/contract/index.tsx
+++ frontend/src/pages/contract/index.tsx
@@ -1,38 +1,19 @@
 import MyContractsForm from "@container/contracts/components/MyContracts";
-import { useEffect, useState } from "react";
-import { contractApi } from "src/api";
-import { MyContracts } from "src/types/MyContracts";
 import Tabs from "@components/profile/Tabs";
 import styled from "styled-components";
-import cookie from "react-cookies";
-import { useRouter } from "next/router";
+import useMyContractsList from "@container/contracts/hooks/useMyContractsList";
 
 function MyContractsList() {
-	const accessToken = cookie.load("access_token");
-	const router = useRouter();
+	const {
+		accessToken,
+		setCurrentTab,
+		tabs,
+		currentTab,
+		onGoingContracts,
+		completedContracts,
+		goToSignIn,
+	} = useMyContractsList();
 
-	const [onGoingContracts, setOnGoingContracts] = useState<MyContracts[]>([]);
-	const [completedContracts, setCompletedContracts] = useState<MyContracts[]>([]);
-	const [currentTab, setCurrentTab] = useState(0);
-
-	const tabs = ["계약중", "계약완료"];
-
-	useEffect(() => {
-		if (accessToken) getMyContracts();
-	}, []);
-
-	const getMyContracts = async () => {
-		const res = await contractApi.getMyContracts();
-		res.data.forEach((contract: MyContracts) => {
-			if (contract.current_level === 4) {
-				setCompletedContracts((prev) => [...prev, contract]);
-			} else setOnGoingContracts((prev) => [...prev, contract]);
-		});
-	};
-
-	const goToSignIn = () => {
-		router.push("/account/signin");
-	};
 	return accessToken ? (
 		<Container>
 			<Tabs setCurrentTab={setCurrentTab} tabs={tabs} />
